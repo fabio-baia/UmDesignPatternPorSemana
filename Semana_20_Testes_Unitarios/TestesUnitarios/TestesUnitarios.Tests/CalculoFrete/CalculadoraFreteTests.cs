02be4ee398ab0fa2abe3d4586d2ab2a5e75afdb7
Fabio Cezar Baía <fabio.baia@benner.com.br>
2017-12-06 00:36:35 -0200

    Calculo PAC

---
+++ Semana_20_Testes_Unitarios/TestesUnitarios/TestesUnitarios.Tests/CalculoFrete/CalculadoraFreteTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TestesUnitarios.CalculoFrete;
+
+namespace TestesUnitarios.Tests.CalculoFrete
+{
+    [TestFixture]
+    public class CalculadoraFreteTests
+    {
+        [Test]
+        public void DeveCalcularFreteViaPACParaDistanciaMenorQue100km()
+        {
+            var calculadoraFrete = new CalculadoraFrete(new FretePAC());
+
+            var resultado = calculadoraFrete.Calcular(50);
+
+            Assert.AreEqual(7.5, resultado);
+        }
+
+        [Test]
+        public void DeveCalcularFreteViaPACParaDistanciaMaiorQue100km()
+        {
+            var calculadoraFrete = new CalculadoraFrete(new FretePAC());
+
+            var resultado = calculadoraFrete.Calcular(150);
+
+            Assert.AreEqual(37.5, resultado);
+        }
+    }
+}
---
+++ Semana_20_Testes_Unitarios/TestesUnitarios/TestesUnitarios/CalculoFrete/CalculadoraFrete.cs
@@ -0,0 +1,38 @@
+﻿namespace TestesUnitarios.CalculoFrete
+{
+    public class CalculadoraFrete : ICalculadoraFrete
+    {
+        ITipoFrete tipoFrete;
+
+        public CalculadoraFrete(ITipoFrete tipoFrete)
+        {
+            this.tipoFrete = tipoFrete;
+        }
+
+        public decimal Calcular(int quilometros)
+        {
+            return tipoFrete.Calcular(quilometros);
+        }
+    }
+
+    public class FretePAC : ITipoFrete
+    {
+        public decimal Calcular(int quilometros)
+        {
+            if (quilometros < 100)
+                return quilometros * 0.15m;
+
+            return quilometros * 0.25m;
+        }
+    }
+
+    public interface ICalculadoraFrete
+    {
+        decimal Calcular(int quilometros);
+    }
+
+    public interface ITipoFrete
+    {
+        decimal Calcular(int quilometros);
+    }
+}
